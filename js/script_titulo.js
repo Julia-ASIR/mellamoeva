6e78a618a5766bfc8574a01ae68e883e026a3ccd
Julia-ASIR <107883846+Julia-ASIR@users.noreply.github.com>
2022-06-21 17:15:29 +0200

    Update script_titulo.js

--- js/script_titulo.js
+++ js/script_titulo.js
@@ -1,3 +1,8 @@
-function titulodinamico() {
-  document.title = 'mellamoeva.es'
+/*
+  Barra de título dinámica - agradecimientos a José Manuel Pérez - Twitter: @jmpevzla.
+  FUNCIONAMIENTO: este script cambia cada X tiempo [valores en milisegundos], el contenido de la etiqueta <title> en un documento HMTL.
+*/
+
+function ScriptTitulo () {
+  setTime (() => document.title = 'Eva: la de Sistemas', 5000)
 }
